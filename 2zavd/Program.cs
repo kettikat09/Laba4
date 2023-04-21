fe25c8a1c0400660f949bd12acba7bed1a8dd1b9
kettikat09 <katya.kryvosheeva.04@gmail.com>
2023-04-21 17:48:19 +0300

    laba4

---
+++ 2zavd/Program.cs
@@ -0,0 +1,89 @@
+﻿//Завдання 2. Транспорт. Визначити ієрархію рухомого складу залізничного транспорту. Створити пасажирський поїзд.
+//Підрахувати загальну чисельність пасажирів і багажу. Провести сортування вагонів поїзда на основі рівня комфортності.
+//Знайти в поїзді вагони, відповідні заданому діапазону параметрів числа пасажирів.
+using Newtonsoft.Json;
+using System;
+using System.Text.Json;
+using static System.Net.Mime.MediaTypeNames;
+
+public class Program
+{
+    public static void Main()
+    {
+        Train t = new Train(5);
+        t.All();
+        Console.WriteLine();
+        t.SortByComfort();
+        t.All();
+        //   t.ShowAllVagonsBellowX(15);
+        Console.WriteLine("Total pasangers: " + Train.TotalPassengers);
+        Console.WriteLine("Total bags: " + Train.TotalBags);
+    }
+}
+public class Train
+{
+    public Vagon[] vagons;
+    public static int TotalPassengers;
+    public static int TotalBags;
+    public Train(int Amount_Of_Vagons)
+    {
+        vagons = new Vagon[Amount_Of_Vagons];
+        Random r = new Random();
+        for (int i = 0; i < vagons.Length; i++)
+        {
+            vagons[i] = new Vagon(r.Next(1, 3), r.Next(1, 40), r.Next(1, 40));
+        }
+        for (int i = 0; i < vagons.Length; i++)
+        {
+            TotalBags += vagons[i].Bags;
+            TotalPassengers += vagons[i].Passengers;
+        }
+
+    }
+    public void SortByComfort()
+    {
+        for (int i = 0; i < vagons.Length; i++)
+        {
+            for (int j = 0; j < vagons.Length; j++)
+            {
+                if (vagons[i].ComfLvl < vagons[j].ComfLvl)
+                {
+                    (vagons[i], vagons[j]) = (vagons[j], vagons[i]);
+                }
+            }
+        }
+    }
+
+    public void ShowAllVagonsBellowX(int X)
+    {
+        for (int i = 0; i < vagons.Length; i++)
+        {
+            if (vagons[i].Passengers < X)
+            {
+                Console.WriteLine(vagons[i].Info());
+            }
+        }
+    }
+
+    public void All()
+    {
+        for (int i = 0; i < vagons.Length; i++)
+        {
+            Console.WriteLine(vagons[i].Info());
+        }
+    }
+}
+public class Vagon
+{
+    public int ComfLvl;
+    public int Passengers;
+    public int Bags;
+    public Vagon(int comfLvl, int passengers, int bags)
+    {
+        ComfLvl = comfLvl;
+        Passengers = passengers;
+        Bags = bags;
+    }
+    public string Info() => $"Comfort LVL: {ComfLvl}, Passangers: {Passengers}, Bags: {Bags}";
+}
+
---
+++ Laba4_2Sem/Program.cs
@@ -0,0 +1,87 @@
+﻿// Завдання 1. Створити об'єкт класу Текст, використовуючи класи Речення, Слово. Методи: доповнити текст, вивести на консоль текст, заголовок тексту.
+
+using Newtonsoft.Json;
+using System;
+using System.Text.Json;
+using static System.Net.Mime.MediaTypeNames;
+
+public class Program
+{
+    public static void Main()
+    {
+        Text t = new Text();
+        t.AddTXT("Hello, I am Tommy.");
+        t.AddTXT("What do you want to know about me?");
+        t.ChangeTop("Intro");
+        Console.WriteLine(t.ToString());
+    }
+}
+public class Text
+{
+    public string Top;
+    public Rechen[] rechen;
+    public Text()
+    {
+        this.rechen = new Rechen[0];
+    }
+    public void AddTXT(string txt)
+    {
+        Rechen[] r = new Rechen[rechen.Length + 1];
+        for (int i = 0; i < rechen.Length; i++)
+        {
+            r[i] = rechen[i];
+        }
+        r[rechen.Length] = new Rechen();
+        r[rechen.Length].AddRechen(txt);
+        rechen = r;
+    }
+    public void ChangeTop(string txt)
+    {
+        Top = txt;
+    }
+    public string ToString()
+    {
+        string a = Top + "\n";
+        for (int i = 0; i < rechen.Length; i++)
+        {
+            a += rechen[i].ToString();
+        }
+        return a;
+    }
+}
+public class Rechen
+{
+    public Slowo[] slowo;
+    public Rechen()
+    {
+        this.slowo = new Slowo[0];
+    }
+    public void AddRechen(string txt)
+    {
+        string[] txt_m = txt.Split(' ');
+        Slowo[] s = new Slowo[txt_m.Length];
+        for (int i = 0; i < s.Length; i++)
+        {
+            s[i] = new Slowo(txt_m[i]);
+        }
+        slowo = s;
+    }
+
+    public string ToString()
+    {
+        string a = "";
+        for (int i = 0; i < slowo.Length; i++)
+        {
+            a += slowo[i].word + " ";
+        }
+        return a;
+    }
+}
+public class Slowo
+{
+    public string word;
+    public Slowo(string word)
+    {
+        this.word = word;
+    }
+}
